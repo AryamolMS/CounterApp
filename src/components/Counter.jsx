050e03ff25f81e87ffb63ea019e9f8ec025cfbd1
AryamolMS <aryamolsimi@gmail.com>
2024-05-30 12:00:06 +0530

    update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
-import './style.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, reset } from '../redux/CounterSlice'
+import React, { useState } from "react";
+import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../redux/CounterSlice";
 
 function Counter() {
-    const dispatch = useDispatch()
-    const count = useSelector((state)=>state.counter.value)
+  const [range,setRange] = useState("")
+  console.log(range);
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
   return (
-<div className="fullscreen-center">
+    <div className="fullscreen-center">
       <div className="card">
         <div className="center-content">
           <h1 className="text-center mb-5 text-info">Counter Application</h1>
-          <h1 className='text mb-5'>{count}</h1>
+          <h1 className="text mb-5">{count}</h1>
           <div className="button-group">
-            <button onClick={()=>dispatch(increment())} className="button increment">Increment</button>
-            <button onClick={()=>dispatch(reset())} className="button reset">Reset</button>
-            <button onClick={()=>dispatch(decrement())} className="button decrement">Decrement</button>
+            <button
+              onClick={() => dispatch(increment(Number(range)))}
+              className="button increment"
+            >
+              Increment
+            </button>
+            <button onClick={() => dispatch(reset(Number(range)))} className="button reset">
+              Reset
+            </button>
+            <button
+              onClick={() => dispatch(decrement(Number(range)))}
+              className="button decrement"
+            >
+              Decrement
+            </button>
           </div>
+        </div>
+        <div>
+          <input type="text" onChange={(e)=>setRange(e.target.value)} className="form-control mb-5" style={{width:'500px',borderColor:'black'}}/>
         </div>
       </div>
     </div>
-)
+  );
 }
 
-export default Counter
+export default Counter;
--- src/redux/CounterSlice.js
+++ src/redux/CounterSlice.js
@@ -7,11 +7,11 @@ export const counterSlice = createSlice({
         value:0
     },
     reducers:{
-        increment:(state)=>{
-            state.value +=1
+        increment:(state,action)=>{
+            state.value +=action.payload
         },
-        decrement:(state)=>{
-            state.value -=1
+        decrement:(state,action)=>{
+            state.value -=action.payload
         },
         reset:(state)=>{
             state.value = 0
